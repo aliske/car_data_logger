ed305593a7f6edc5318d27df3dc40c615566d819
Aaron Liske <aaron.liske@gmail.com>
2020-06-27 09:58:44 -0400

    MySQL insert Query
    Added the insert query for mysql data saving.

--- Mysql_Connector.java
+++ Mysql_Connector.java
@@ -31,6 +31,29 @@ public class Mysql_Connector {
 		return true;
 	}
 	
+	public boolean save_data(String pid1, String response1, String pid2, String response2, String pid3, String response3, String pid4, String response4, String pid5, String response5, String pid6, String response6) {
+		try {
+			preparedStatement = connect.prepareStatement("INSERT INTO " + UI_Screen_Main.database_name_text.getText() + ".ecu_data VALUES (default, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			preparedStatement.setString(1, pid1);
+			preparedStatement.setString(2, response1);
+			preparedStatement.setString(3, pid2);
+			preparedStatement.setString(4, response2);
+			preparedStatement.setString(5, pid3);
+			preparedStatement.setString(6, response3);
+			preparedStatement.setString(7, pid4);
+			preparedStatement.setString(8, response4);
+			preparedStatement.setString(9, pid5);
+			preparedStatement.setString(10, response5);
+			preparedStatement.setString(11, pid6);
+			preparedStatement.setString(12, response6);
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			AlertBox.display("MySQL Error", e.toString());
+			return false;
+		}
+		return true;
+	}
+	
 	public void close() {
 		try {
 			if(statement != null) {
